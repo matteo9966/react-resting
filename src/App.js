db14710833b3a96acdbee9962195511b3c135284
matteo9966 <matteoluigitommasi@gmail.com>
2022-04-21 16:09:41 +0200

    first test added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+  const [color, setColor] = useState("red");
+  const title = color === "red" ? "Change to blue" : "Change to red";
   return (
     <div className="App">
-      <button style={{ backgroundColor: "red" }}>Change to blue</button>
+      <button
+        style={{ backgroundColor: color }}
+        onClick={() => {
+          setColor((color) => {
+           return color === "red" ? "blue" : "red";
+          });
+        }}
+      >
+        {title}
+      </button>
     </div>
   );
 }
